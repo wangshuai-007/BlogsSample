f94e71cf374eedfce28ee1990690377124be2e1b
我零0七 <007@wangshuai.app>
2021-09-26 17:12:56 +0800

    add readme

--- DevPropertyGridControlExpandCollection/DevPropertyGridControlExpandCollection/Properties/AssemblyInfo.cs
+++ DevPropertyGridControlExpandCollection/DevPropertyGridControlExpandCollection/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExpandCollection")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("devexpress如何在PropertyGridControl中直接编辑集合成员")]
+[assembly: AssemblyDescription("此为博客：https://blog.wangshuai.app/2021/09/26/devexpress%E5%A6%82%E4%BD%95%E5%9C%A8PropertyGridControl%E4%B8%AD%E7%9B%B4%E6%8E%A5%E7%BC%96%E8%BE%91%E9%9B%86%E5%90%88%E6%88%90%E5%91%98/的源码")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExpandCollection")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 王帅 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ DevPropertyGridControlExpandCollection/readme.md
@@ -0,0 +1,4 @@
+此项目为文章[devexpress如何在PropertyGridControl中直接编辑集合成员](https://blog.wangshuai.app/2021/09/26/devexpress%E5%A6%82%E4%BD%95%E5%9C%A8PropertyGridControl%E4%B8%AD%E7%9B%B4%E6%8E%A5%E7%BC%96%E8%BE%91%E9%9B%86%E5%90%88%E6%88%90%E5%91%98/)的代码
+
+.NET 版本为 `4.7.2`，devexpress 版本为`18.1`
+
